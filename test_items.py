3af6fbbe7a68ecf32f16516a0a8daa4e6b71855f
Daniil-Auto <70564934+Daniil-Auto@users.noreply.github.com>
2020-09-14 16:56:11 +0700

    stepik home

---
+++ test_items.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+   
+def test_correct(browser):
+    
+    browser.get(link)
+    browser.implicitly_wait(5)
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), f"Button missing"
+   
