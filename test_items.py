122ac7d57d2da69b647f67581b4c494830128457
Daniil-Auto <70564934+Daniil-Auto@users.noreply.github.com>
2020-09-14 17:16:14 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
    
-def test_correct(browser):
+def test_check_basket_button_exists(browser):
     
     browser.get(link)
     browser.implicitly_wait(5)
